759485670dacd09981e145ec71e30e0a38528ee2
Tanjilur Rahman Nabil <nabil_metalboy@hotmail.com>
2023-10-09 00:35:51 +0600

    BlogPost

---
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,75 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            var blogPost = new BlogPost
+            {
+                Title = request.Title,
+                Author = request.Author,
+                ShortDescription = request.ShortDescription,
+                Content = request.Content,
+                UrlHandle = request.UrlHandle,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                PublishedDate = request.PublishedDate,
+
+            };
+            blogPost = await blogPostRepository.CreateAsync(blogPost);
+
+            //Convert to DTO
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Title = blogPost.Title,
+                Author = blogPost.Author,
+                UrlHandle = blogPost.UrlHandle,
+                ShortDescription = blogPost.ShortDescription,
+                Content = blogPost.Content,
+                PublishedDate = blogPost.PublishedDate,
+                IsVisible = blogPost.IsVisible,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl
+            };
+            return Ok(response);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllBlogPosts()
+        {
+            var blogPosts= await blogPostRepository.GetAllAsync();
+            //Convert to DTO
+            var response = new List<BlogPostDto>();
+            foreach (var blogPost in blogPosts)
+            {
+                response.Add(new BlogPostDto
+                {
+                    Id = blogPost.Id,
+                    Title = blogPost.Title,
+                    Author = blogPost.Author,
+                    UrlHandle = blogPost.UrlHandle,
+                    ShortDescription = blogPost.ShortDescription,
+                    Content = blogPost.Content,
+                    PublishedDate = blogPost.PublishedDate,
+                    IsVisible = blogPost.IsVisible,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl
+                });
+            }
+            return Ok(response);
+        }
+    }
+}
