897dc101e67eb1009c353374fe2d5e3876a08a8e
Tanjilur Rahman Nabil <nabil_metalboy@hotmail.com>
2023-10-24 23:00:36 +0600

    ImageUpload and display as static file in browser

---
+++ CodePulse.API/Controllers/ImagesController.cs
@@ -0,0 +1,65 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Implementation;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file, [FromForm] string fileName, [FromForm] string title)
+        {
+            ValidateFileUpload(file);
+            if(ModelState.IsValid)
+            {
+                //File Upload
+                var blogImage = new BlogImage
+                {
+                    FileExtension = Path.GetExtension(file.FileName).ToLower(),
+                    Title = title,
+                    FileName = fileName,
+                    DateCreated = DateTime.Now,
+                };
+               blogImage = await imageRepository.Upload(file, blogImage);
+                //Convert to DTO
+                var response = new BlogImageDto
+                {
+                    Id = blogImage.Id,
+                    Title = blogImage.Title,
+                    DateCreated = blogImage.DateCreated,
+                    FileName = blogImage.FileName,
+                    FileExtension = blogImage.FileExtension,
+                    Url = blogImage.Url,
+                };
+                return Ok(response);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(IFormFile file)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if(!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower()))
+            {
+                ModelState.AddModelError("file", "Unsupported file format");
+            }
+            if (file.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size cannot be more than 10MB");
+
+            }
+        }
+    }
+}
--- CodePulse.API/Program.cs
+++ CodePulse.API/Program.cs
@@ -2,12 +2,14 @@ using CodePulse.API.Data;
 using CodePulse.API.Repositories.Implementation;
 using CodePulse.API.Repositories.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +20,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
 builder.Services.AddScoped<IBlogPostRepository,BlogPostRepository>();
+builder.Services.AddScoped<IImageRepository,ImageRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,7 +38,12 @@ app.UseCors(options =>
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+//This will aloow access to a static file from a folder within api
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"Images")),
+    RequestPath = "/Images"
+});
 app.MapControllers();
 
 app.Run();
