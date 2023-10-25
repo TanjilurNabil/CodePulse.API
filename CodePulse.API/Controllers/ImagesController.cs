a9f79929fab67eeaf7a6b265ad28130a13ad4577
Tanjilur Rahman Nabil <nabil_metalboy@hotmail.com>
2023-10-26 00:12:07 +0600

    fetch all images

--- CodePulse.API/Controllers/ImagesController.cs
+++ CodePulse.API/Controllers/ImagesController.cs
@@ -17,6 +17,25 @@ namespace CodePulse.API.Controllers
         {
             this.imageRepository = imageRepository;
         }
+        [HttpGet]
+        public async Task<IActionResult> GetAllImages()
+        {
+            var images = await imageRepository.GetAll();
+            var response = new List<BlogImageDto>();
+            foreach (var image in images)
+            {
+                response.Add(new BlogImageDto
+                {
+                    Id = image.Id,
+                    Title = image.Title,
+                    DateCreated = image.DateCreated,
+                    FileName = image.FileName,
+                    FileExtension = image.FileExtension,
+                    Url = image.Url,
+                });
+            }
+            return Ok(response);
+        }
         [HttpPost]
         public async Task<IActionResult> UploadImage([FromForm] IFormFile file, [FromForm] string fileName, [FromForm] string title)
         {
